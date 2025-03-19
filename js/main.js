051188ac94013552c4768873632584f50dc4aedf
nbriz <nickbriz@protonmail.com>
2025-03-19 18:28:20 -0500

    testing animations

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ const colors = [
   ['#ff0000', '#ff0099', '#ff00ff', '#6600ff', '#0000ff', '#0099ff', '#00ffff', '#00ff66', '#00ff00', '#99ff00', '#ffff00', '#ff6600'],
   ['#993333', '#993366', '#993399', '#663399', '#333399', '#336699', '#339999', '#339966', '#339933', '#669933', '#999933', '#996633']
 ]
+
+
+nn.get('#about-link').on('click', () => {
+  nn.get('.splash-intro').css({ left: '100vw' })
+  nn.get('.case-study').css({ left: '100vw' })
+})
+
+nn.get('#init-link').on('click', () => {
+  nn.get('.splash-intro').css({ left: '0vw' })
+  nn.get('.case-study').css({ left: '100vw' })
+})
+
+nn.get('#dream-link').on('click', () => {
+  nn.get('.case-study').css({ left: '25vw' })
+})
